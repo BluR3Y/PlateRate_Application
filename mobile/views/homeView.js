8eadffff575bd75aa2acdfac613dc14636b43363
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-06-15 01:11:05 -0400

    svg packages and dependencies installed and setup

--- mobile/views/homeView.js
+++ mobile/views/homeView.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { View, Text, ScrollView, Button, Image } from 'react-native';
 import styles from '../styles/homeStyles';
 
+import { SvgUri } from 'react-native-svg';
+
+import Logo from '../content/images/platerate_logo.svg';
+
 export function HomeView({ navigation }) {
     return (
         <ScrollView>
             <View style={styles.header}>
-
+                <SvgUri
+                    width='10%'
+                    height='100%'
+                    uri='http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg'
+                />
+                <Logo 
+                    
+                />
             </View>
         </ScrollView>
     )
