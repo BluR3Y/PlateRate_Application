6767e913d8a5a30fef87d48540e3dd00a879ea6d
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-06-15 02:28:01 -0400

    rating filters incorporated

--- mobile/styles/homeStyles.js
+++ mobile/styles/homeStyles.js
@@ -6,7 +6,52 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 65,
         backgroundColor: '#FFFFFF',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    headerLogo: {
+        width: 140,
+        height: 35,
+        marginLeft: 20,
+    },
+    headerAward: {
+        width: 40,
+        height: 30,
+        position: 'absolute',
+        right: 0,
+        marginRight: 80
+    },
+    ratingFilters : {
+        width: '100%',
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+    }, ratingFilterItem: {
+        width: 'auto',
+        height: 30,
+        paddingLeft: 5,
+        paddingRight: 5,
+        flexDirection: 'row',
+        alignItems: "center",
+        marginLeft: 10,
+        borderTopEndRadius: 12,
+        borderBottomEndRadius: 12,
+        borderTopStartRadius: 12,
+        borderBottomStartRadius: 12,
+    },
+    checkMark: {
+        backgroundColor: 'green',
+        width: 18,
+        height: 18,
+        borderRadius: 50,
+        flexDirection: 'column',
+        alignContent:'center',
+        justifyContent: 'center',
+    },
+    ratingFilterText: {
+        marginLeft: 5,
+        fontSize: 15,
+        color: '#494949',
     }
 });
 
--- mobile/views/homeView.js
+++ mobile/views/homeView.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { View, Text, ScrollView, Button, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 import styles from '../styles/homeStyles';
 
-import { SvgUri } from 'react-native-svg';
+import CheckMark from '../content/images/check_mark.svg';
 
-import Logo from '../content/images/platerate_logo.svg';
+const RatingFilter = (props) => {
+
+    const [isEnabled, setIsEnabled] = useState(true);
+
+    const onPress = () => {
+        setIsEnabled(!isEnabled);
+    }
+
+    return(
+        <TouchableOpacity style={styles.ratingFilterItem} onPress={onPress}>
+            <View style={[styles.checkMark, {opacity: isEnabled ? 1 : 0}]}>
+                <CheckMark width={18} height={13} fill={'#FFF'}/>
+            </View>
+            <Text style={styles.ratingFilterText}>{props.filterName}</Text>
+        </TouchableOpacity>
+    )
+}
 
 export function HomeView({ navigation }) {
     return (
         <ScrollView>
             <View style={styles.header}>
-                <SvgUri
-                    width='10%'
-                    height='100%'
-                    uri='http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg'
+                <Image
+                    style={styles.headerLogo}
+                    source={require('../content/images/platerate_logo.png')}
                 />
-                <Logo 
-                    
+                <Image
+                    style={styles.headerAward}
+                    source={require('../content/images/award.png')}
                 />
+            </View>
+            <View style={styles.ratingFilters}>
+                <RatingFilter filterName='Pickup'/>
+                <RatingFilter filterName='Delivery'/>
+                <RatingFilter filterName='Order In/Ahead'/>
             </View>
         </ScrollView>
     )
